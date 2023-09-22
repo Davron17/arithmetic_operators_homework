563dcd5f716873f934a220a1803e233721bcd3a1
Davron17 <dorazaliyev75@gmail.com>
2023-09-22 22:41:50 +0500

    5-misol

--- arithmetic_op05.py
+++ arithmetic_op05.py
@@ -1,5 +1,6 @@
 #Create a variable called 'number' with data type int.
-
+number=int(input())
 #Create a variable called 'answer' and assign the remainder of the division of number by 3 to it.
-
+answer=number//100+number%10*100+(number-number//100*100)//10*10
 #Print 'answer'.
+print(answer)
